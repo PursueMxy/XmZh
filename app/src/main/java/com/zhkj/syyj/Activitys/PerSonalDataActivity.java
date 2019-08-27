61d83b6a36e4385cecb02702bbe56f44f1bfa220
zhkj8 <971679951@qq.com>
2019-08-27 11:25:26 +0800

    456

---
+++ app/src/main/java/com/zhkj/syyj/Activitys/PerSonalDataActivity.java
@@ -0,0 +1,15 @@
+package com.zhkj.syyj.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhkj.syyj.R;
+
+public class PerSonalDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_per_sonal_data);
+    }
+}
